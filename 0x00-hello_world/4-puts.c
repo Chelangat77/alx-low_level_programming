76a780e5d85092bd37c0ca6ed062927ad027e511
Chelangat77 <cleitich@live.com>
2023-02-16 09:40:41 -0800

    intro to C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main () {
-char str[] = "Programming is like building a multilingual puzzle";
-puts(str);
-return 0;
+/**
+ * main - Entry point
+ *
+ * Description: 'the programmers description'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
 }
