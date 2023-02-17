5795aed90e9b298d53f24beea289a53ac948d96e
Chelangat77 <cleitich@live.com>
2023-02-17 07:52:31 -0800

    esle if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
-/* main - Entry point */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *Return: Always 0 (Success)
+ *
+ */
 int main(void)
 
 {
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
