3dd3eb62174e1d47813176faf3573a9cc19af100
Chelangat77 <cleitich@live.com>
2023-02-21 12:32:34 -0800

    if else

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * int _isalpha - Entry point
+ *
+ * Description: checks is a character is lowercase with cinditional if
+ *
+ * @c: the integer value it receives
+ *
+ * Return: 1 if true. 0 if false.
+ */
+int _isalpha(int c);
+{
+	if (c > == 'a' && c == < 'z')
+	{
+		return (1);
+	}
+	else if (c > == 'A' && c == < 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
