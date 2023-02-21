1f8817db276b0c8c5ce84ca7512416d66869e582
Chelangat77 <cleitich@live.com>
2023-02-21 07:38:25 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0 - Success
+ */
+int main(void)
+
+
+	for (i = 97; i < 122; i++)
+
+{
+	putchar(i);
+	putchar(\n);
+	return (0);
+}
