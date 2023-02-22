fb8a3b7fd95c4cb2c09a9a88a23e574f7636dd27
Chelangat77 <cleitich@live.com>
2023-02-22 10:23:10 -0800

    last

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_to_98 - main entry point
+ * @n: variable
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			_putchar('a');
+			if (a != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = n; a >= 98; a--)
+		{
+			_putchar('a');
+			if (a != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
