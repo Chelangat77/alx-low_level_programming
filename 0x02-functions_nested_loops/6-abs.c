f0504154111cf099f4c89d4c3c503c06c2cfe90f
Chelangat77 <cleitich@live.com>
2023-02-22 06:50:48 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - main entry point
+ * description: function generates absolute value
+ * @i: i is an integer
+ * Return: void
+ */
+
+int _abs(int i)
+{
+i = _abs;
+
+	_putchar('i');
+}
