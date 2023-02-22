50ff843bd6252bc6a1e97eabb6ce7ec6f6ac650f
Chelangat77 <cleitich@live.com>
2023-02-22 09:18:06 -0800

    time

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main entry point
+ * Return: void
+ * Description: function that prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int t;
+	int m;
+
+	for (t = 0; t < 24; t++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((t / 10) + '0');
+			_putchar((t % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
