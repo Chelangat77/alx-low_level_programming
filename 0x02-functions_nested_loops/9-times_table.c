f493a0f258f631169d3cce2da7865a9b38b9de62
Chelangat77 <cleitich@live.com>
2023-02-22 09:32:52 -0800

    time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,19 +8,40 @@
 
 void jack_bauer(void)
 {
-	int t;
-	int m;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (t = 0; t < 24; t++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		for (m = 0; m < 60; m++)
+		if (hours_tens == '2')
 		{
-			_putchar((t / 10) + '0');
-			_putchar((t % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
