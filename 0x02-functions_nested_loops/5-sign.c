b19bc223cc4ad0641406a8d911d557d011ca7d37
Chelangat77 <cleitich@live.com>
2023-02-22 05:57:04 -0800

    digits

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - main entry point
+ *
+ * the function indicates if the value is positive or negative
+ *
+ * @n: is the integer value it receives
+ *
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+	_putchar('+');
+
+	return (1);
+}
+
+if (n < 0)
+{
+	_putchar('-');
+
+	return (-1);
+}
+	return (0);
+}
