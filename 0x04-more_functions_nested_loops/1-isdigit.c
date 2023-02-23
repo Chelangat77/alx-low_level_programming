506d6db751e2bc4d49048ad68a33c0afe01619c2
Chelangat77 <cleitich@live.com>
2023-02-23 05:23:59 -0800

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - main entry point
+ * @c: c is the integer
+ * Return: return 1 if true else return 0
+ * Description: function identifies digits 0 to 9
+ */
+
+int _isdigit(int c)
+{
+int a = 0;
+	for (a = 0; a <= 9; a++)
+	{
+		return (1);
+	}
+return (0);
+}
+
