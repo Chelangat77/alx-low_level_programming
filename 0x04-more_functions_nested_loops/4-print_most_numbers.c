28e4b9df8897ba3f0468e40d5a7bed531be2a6d4
Chelangat77 <cleitich@live.com>
2023-02-23 07:47:29 -0800

    next

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,13 @@ void print_most_numbers(void)
 {
 int i = 0;
 
-for (i = 0; i <= 9; i++)
-{
-	if (i != 2 && i != 4)
+	while (i <= 9)
 	{
-		_putchar(i + '0');
-	}
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 		i++;
-}
-_putchar('\n');
+	}
+	_putchar('\n');
 }
