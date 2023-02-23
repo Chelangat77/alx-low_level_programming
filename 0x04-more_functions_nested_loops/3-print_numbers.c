bf02f044d5c013269721c642e79772da78775ef8
Chelangat77 <cleitich@live.com>
2023-02-23 06:35:33 -0800

    next line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - main entry point
+ * Descritpion: function prints numbers from 0 to 9 followed by new line
+ * Return: return void
+ *
+ */
+
+void print_numbers(void)
+{
+char i = 0;
+for (i = 0; i <= 9; i++)
+{
+	_putchar(i);
+	i++;
+}
+{
+	_putchar('\n');
+}
+}
