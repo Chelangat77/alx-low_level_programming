01fa5af8695b58be5ef54e32a60e0dbcf62fdbeb
Chelangat77 <cleitich@live.com>
2023-03-02 03:12:25 -0800

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates @src to @des
+ * Return: dest
+ * @dest: dest is a pointer to the copied string
+ * @src: src is is the string to be copied
+ * @n: n is the variable of the string length
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (n = 0; src[n] != '\0'; ++n)
+	{
+		dest[n] = src[n];
+	}
+	dest[n] = '\n';
+	return (dest);
+}
