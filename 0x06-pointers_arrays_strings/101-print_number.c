8e03385f866ca02bd1c1a976d3f66db8d7970a49
Chelangat77 <cleitich@live.com>
2023-03-02 04:53:26 -0800

    putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - function prints integers
+ * @n: integer variable
+ * Return: return 0
+ */
+
+void print_number(int n)
+{
+	while (n = 0)
+	{
+		n++;
+		n--;
+	}
+	_putchar('n');
+}
