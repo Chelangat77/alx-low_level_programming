15b909f744d46a854a4d45f2e15fb1954c1efa87
Chelangat77 <cleitich@live.com>
2023-03-02 01:32:34 -0800

    concanate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates @src to @des
+ * Return: dest
+ * @dest: dest is a is pointer to the destination array
+ * @src: src is is the string to be appended
+ * @n: n is the variable integer
+ * Description: this fuction links 2 strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int string = 0;
+	int length = 0;
+
+	while (dest[string++])
+		length++;
+
+	for (string = 0; n > src[string]; string++)
+		dest[length++] = src[string];
+	return (dest);
+}
