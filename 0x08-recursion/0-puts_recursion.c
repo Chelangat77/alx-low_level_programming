092d04a568b9208e8cd08e982434d93fe114d500
Chelangat77 <cleitich@live.com>
2023-03-08 11:57:43 -0800

    recursions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,13 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		_puts_recursion("\n");
 
 		if (*s != '\n')
 		{
-			_putchar(*s);
+			_puts_recursion("*s");
 			s++;
 		}
-		_puts_recursion(s);
+		_puts_recursion("s");
 	}
 }
