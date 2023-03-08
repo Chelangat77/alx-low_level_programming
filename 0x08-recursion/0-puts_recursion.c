30d2f9d6058443258ba257ce7c47f9e86bdcb82d
Chelangat77 <cleitich@live.com>
2023-03-08 12:16:06 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\n')
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		if (*s != '\0')
 		{
-			_puts_recursion("*s");
-			s++;
+			_puts_recursion(s);
 		}
-		_puts_recursion("s");
-
-		if (*s == '\0')
-        	{
-                	_puts_recursion("\n");
-		}
+		if (*s == '\n')
+			_putchar('\n');
+	}
 }
