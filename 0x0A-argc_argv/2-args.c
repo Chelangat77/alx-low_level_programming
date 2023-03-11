fe2c8b75d316cb47dc13d4bffb0f15a439b82c64
Chelangat77 <cleitich@live.com>
2023-03-11 07:02:33 -0800

    print string

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argv: holds array of arguments
+ * @argc: tracks the number of arguments
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+while (argv = 0)
+{
+	argv++;
+	*argv++;
+	printf("%s\n", *argv);
+}
+return (0);
+}
