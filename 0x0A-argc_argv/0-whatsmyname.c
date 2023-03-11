fcb1b2850537e14b1745b2d9d4457b9b550688b6
Chelangat77 <cleitich@live.com>
2023-03-11 05:22:36 -0800

    agv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -prints the name eof the program
+ * @ argc: number of command lines
+ * @argv: array of string
+ * Return: return 0
+ */
+
+int main(int argc, char* argv[])
+{
+	argv[0];
+
+	printf("%s\n", argc[argv]);
+
+return (0);
+}
