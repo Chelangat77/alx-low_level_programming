c92b56f0f79411469e9004462fdd092b5dfa65e5
Chelangat77 <cleitich@live.com>
2023-03-22 13:03:36 -0700

    pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,12 +10,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
+	size_t i;
 
-	while (size-- > 0)
+	if (array && action)
 	{
-		action(*array);
-		array++;
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
