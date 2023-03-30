8404ad84d7065138ccaae44f8581943d75eeb66a
Chelangat77 <cleitich@live.com>
2023-03-30 11:58:52 -0700

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int print(void);
+
+/**
+ */
+
+int print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
