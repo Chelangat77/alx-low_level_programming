08a16fa9fa3457495a63fa07d3482928c389d375
Chelangat77 <cleitich@live.com>
2023-03-30 10:47:37 -0700

    length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,8 +15,8 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-			return (1);
-		while(h != NULL)
+
+		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		count++;
