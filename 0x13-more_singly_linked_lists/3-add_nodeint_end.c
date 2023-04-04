f5d91169735437f8148bd41fd6bd5ff9127928d3
Chelangat77 <cleitich@live.com>
2023-04-04 05:40:21 -0700

    end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - adds a new node at the end
+ * add_nodeint_end - adds a new node at the end
  * @head: pointer to pointer of the structure
  * @n: variable integer
  * Return: the address of the new element, or NULL if it failed
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *add, *end;
 
